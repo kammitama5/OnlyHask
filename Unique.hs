1d12e253bc54755128c25d601a81761a0d1706d1
kammitama5 <krystal.maughan@gmail.com>
2018-08-30 20:09:45 -0700

    challenge_1

---
+++ Unique.hs
@@ -0,0 +1,28 @@
+module Unique where
+
+-- given a string, see if it has unique characters
+-- questions: do we care about spaces or special chars?
+-- what sort of time complexity do we want ideally?
+-- what sort of data structures am I limited to using
+
+
+
+-- tests
+-- "abba" should return False
+-- "abc" should return True
+-- "" should return False
+-- "dddedd" should return False
+-- "abracadabra" should return False
+-- "a" should return True
+
+-- idea: find set of the string and compare it to the original
+-- first case: empty string must not return True
+
+-- Type: takes a string, returns a Bool
+-- recurse through unique string and check if x has been seen in list. If so then
+-- list is not unique. If elements have not been seen, list is unique.
+
+uniquestring :: String -> Bool
+uniquestring [] = False
+uniquestring (x:xs) = if x `elem` xs || uniquestring xs then False else True
+
